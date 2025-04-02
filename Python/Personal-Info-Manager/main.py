00fc4d1f5cf45af969d676629ff196787c9b8176
20146307 <20146307@tafe.wa.edu.au>
2025-04-02 20:55:39 +0800

    feat: prompt user for basic person data
    add 4 variables, first_name, last_name, age, height.
    age will calculate how old the indiviual will be in 5 years
    and will print it as a string.
    height will convert from cm to m and print as a string.
    first_name and last_name will print in a greeting message.

--- Python/Personal-Info-Manager/main.py
+++ Python/Personal-Info-Manager/main.py
@@ -1,1 +1,15 @@
 # Personal Info Manager - collects and stores basic user information
+
+
+first_name = input("First Name:> ")
+last_name = input("Last Name:> ")
+age = int(input("Age:> "))
+height = int(input("Height(cm):> "))
+
+age = age + 5
+
+height = height / 100
+
+print(f"\nHow are you today {first_name}, {last_name}")
+print(f"\nIn five years you will be {age}")
+print(f"\nYou are {height}m.")
